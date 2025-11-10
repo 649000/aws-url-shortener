ecbc5cd7a3ff95c05f897f59b394fe0e313d6fcf
Nazri <649000@users.noreply.github.com>
2025-11-10 10:55:09 +0800

    fix: remove unused imports

--- src/main/java/com/nazri/urlshortener/exception/GlobalExceptionMapper.java
+++ src/main/java/com/nazri/urlshortener/exception/GlobalExceptionMapper.java
@@ -1,13 +1,11 @@
 package com.nazri.urlshortener.exception;
 
 import com.nazri.urlshortener.dto.ErrorDTO;
-import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+
 import java.util.logging.Logger;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Provider
 public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
